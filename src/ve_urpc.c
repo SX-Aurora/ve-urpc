7d78763d9b5b4fa661086fe0c6b7188150542693
Erich Focht <efocht@gmail.com>
2021-11-09 17:53:53 +0100

    Added LGPL headers.

--- src/ve_urpc.c
+++ src/ve_urpc.c
@@ -1,9 +1,22 @@
 /**
- * TODO: add License header
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
  *
  * VE specific parts of VE-URPC.
  * 
- * (C)opyright 2020 Erich Focht
+ * Copyright (c) 2020 Erich Focht
  */
 
 #include <stdlib.h>
